c8690da6c82f522b31f78549bea9ad86611c7087
Dustin Murdock <dmurdock2@gmail.com>
2020-02-21 15:08:36 -0600

    added Owners, Locations, and Employees sections for Chapter 2

--- src/components/employee/EmployeeCard.js
+++ src/components/employee/EmployeeCard.js
@@ -4,9 +4,30 @@ import React from "react";
 
 const EmployeeCard = () => {
     return (
-        <div>
-            
-        </div>
+        <div className="allEmployees">
+            <div className="card">
+                <div className="card-content">
+                    <picture>
+                    <img src={require("./Angie.jpg")} alt="Angie" />
+                    </picture>
+                    <h6>picture used from https://generated.photos/</h6>
+                    <h3>
+                    Employee: <span className="cardEmployeeName">Angie</span>
+                    </h3>
+                </div> 
+            </div>
+            <div className="card">
+                <div className="card-content">
+                    <picture>
+                    <img src={require("./Rosa.jpg")} alt="Rosa" />
+                    </picture>
+                    <h6>picture used from https://generated.photos/</h6>
+                    <h3>
+                    Employee: <span className="cardOwnerName">Rosa</span>
+                    </h3>
+                </div> 
+            </div>
+        </div>       
     )
 
 }
--- src/components/owner/OwnerCard.js
+++ src/components/owner/OwnerCard.js
@@ -3,9 +3,30 @@ import React from "react";
 
 const OwnerCard = () => {
     return (
-        <div>
-            
-        </div>
+        <div className="allOwners">
+            <div className="card">
+                <div className="card-content">
+                    <picture>
+                    <img src={require("./Susan pic.jpg")} alt="Susan" />
+                    </picture>
+                    <h6>picture used from https://generated.photos/</h6>
+                    <h3>
+                    Owner: <span className="cardOwnerName">Susan</span>
+                    </h3>
+                </div> 
+            </div>
+            <div className="card">
+                <div className="card-content">
+                    <picture>
+                    <img src={require("./Ronpic.jpg")} alt="Ron" />
+                    </picture>
+                    <h6>picture used from https://generated.photos/</h6>
+                    <h3>
+                    Owner: <span className="cardOwnerName">Ron</span>
+                    </h3>
+                </div> 
+            </div>
+        </div>    
     )
 }
 
