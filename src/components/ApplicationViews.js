8b6f9a36653a8b3d2dd708ec554fecd8da5a5fe6
Dustin Murdock <dmurdock2@gmail.com>
2020-02-24 12:57:09 -0600

    added in database with Animal entries

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,11 +1,12 @@
 import { Route } from "react-router-dom";
 import React from "react";
 import Home from "./home/Home";
-import AnimalCard from "./animal/AnimalCard";
+import AnimalList from "./animal/AnimalList";
 //only include these once they are built - previous practice exercise
 import LocationCard from "./locations/LocationCard";
 import EmployeeCard from "./employee/EmployeeCard";
 import OwnerCard from "./owner/OwnerCard";
+
 
 const ApplicationViews = () => {
   return (
@@ -17,11 +18,9 @@ const ApplicationViews = () => {
           return <Home />;
         }}
       />
-      <Route
-        path="/animals"
-        render={props => {
-          return <AnimalCard />;
-        }}
+      <Route path="/animals" render={(props) => {
+          return <AnimalList />
+        }} 
       />
       <Route
         path="/locations"
