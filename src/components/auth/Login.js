9eeb9de81a8c2ad5f6cc78e41b21c93ff47b4844
Dustin Murdock <dmurdock2@gmail.com>
2020-03-03 11:29:56 -0600

    lifted state of user to Kennel.js to allow for only Login and Locations link to be visible prior to login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,8 +2,8 @@ import React, { useState } from "react"
 
 const Login = props => {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
+  console.log(props)
 
-  // Update state whenever an input field is edited
   const handleFieldChange = (evt) => {
     const stateToChange = { ...credentials };
     stateToChange[evt.target.id] = evt.target.value;
@@ -12,15 +12,7 @@ const Login = props => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    /*
-        For now, just store the email and password that
-        the customer enters into session storage.
-        ...Let's just trust the user... That's a good idea, right????
-    */
-    sessionStorage.setItem(
-      "credentials",
-      JSON.stringify(credentials)
-    );
+    props.setUser(credentials)
     props.history.push("/home");
   }
 
